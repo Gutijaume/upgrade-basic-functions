3a68e479315cb2f14a7e51862e2f940e615c9023
gutijaume <gutijaume@hotmail.com>
2021-12-07 20:02:09 +0100

    Create iteracion#7.js
    Con este buscador de nombres puedo localizar el nombre y en que posición está

---
+++ scripts/iteracion#7.js
@@ -0,0 +1,25 @@
+const nameFinder = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+  ];
+  
+let finderName = (data, nombre) =>{
+    for (let i = 0; i < data.length; i++){
+        if (founded = data[i].includes(nombre)){ 
+            console.log(founded)
+        } else {console.log("Name not founded")}
+    }
+
+}
+
+finderName (nameFinder, "Tony" ) //En este parámetro buscaremos el nombre
+
