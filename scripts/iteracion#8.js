1675f9e3d41e21d67be5d29fee5340c04d9af6de
gutijaume <gutijaume@hotmail.com>
2021-12-08 11:48:23 +0100

    Update iteracion#8.js
    No lo he conseguido, sigo avanzando para no estancarme

--- scripts/iteracion#8.js
+++ scripts/iteracion#8.js
@@ -14,19 +14,18 @@ const counterWords = [
   
 
 
-let repeatCounter = (data) =>{
-    for (let i =0; i<data.length; i++){
-        let count = 0;
-        if (data[i] === data [i+1]){
-            count += 1
-        } if (count >= 2) {
-            console.log (data[i] + " is " + count + " times repeated")
-        } else {console.log(data[i] + " is not repeated")}
-    }
-}
 
-repeatCounter (counterWords)
-
-
-
-
+  
+  function cuantosRepetidos(array, elemento) {//Aquí le llamo elemento
+      var contador = 0;
+      for (var i = 0; i < array.length; i++) {
+          for(item of array[i]){/*Aquí le llamo item para no pisar la variable,
+   además hago el loop sobre array[i]!*/
+              if(item[i] === elemento[i]){
+                  contador++;
+              }
+          }
+      }
+      return contador;
+  }
+  console.log(cuantosRepetidos(counterWords, 'sleep'));
